78e146d3e61172b1d8d23248f2e18cc515613278
urooj Nadeem <56505998+urooj19@users.noreply.github.com>
2023-12-30 14:26:32 +0500

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,12 @@
+
+<?php
+$con = mysqli_connect("localhost", "i8774292_wp3", "Q.mpDnF0pgMAGW90pqH05");
+mysqli_select_db($con,'i8774292_wp3');
+if($con)
+{
+   // echo "Connection successful";
+}
+else{
+    // echo "error in connection";
+}
+?>
---
+++ database.php
@@ -0,0 +1,12 @@
+
+<?php
+$con = mysqli_connect("localhost", "i8774292_wp3", "C.w0w85FEDHhvhlJ5mg53");
+mysqli_select_db($con,'i8774292_wp2');
+if($con)
+{
+   // echo "Connection successful";
+}
+else{
+    // echo "error in connection";
+}
+?>
